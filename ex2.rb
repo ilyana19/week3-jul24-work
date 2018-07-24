67e863c35a8482d9e9ac6590db56bd0555cf293c
ilyana19 <aries_2152@hotmail.com>
2018-07-24 11:01:51 -0400

    finished ex3

--- ex2.rb
+++ ex2.rb
@@ -1,7 +1,22 @@
 class Person
   def initialize(name, emotions)
     @name = name
-    @emotion = emotions
+    @emotions = emotions
+  end
+
+  def status
+    @emotions.each do |k, v|
+      case v
+      when 3
+        puts "I'm feeling a high amount of #{k}"
+      when 2
+        puts "I'm feeling a medium amount of #{k}"
+      when 1
+        puts "I'm fee,ling a low amount of #{k}"
+      else
+        puts "ERROR"
+      end
+    end
   end
 end
 
@@ -12,3 +27,5 @@ linda = Person.new("Linda", emotions = {
 })
 
 p linda
+
+linda.status
