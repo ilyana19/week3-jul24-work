21465d1a2efd075b6834073c393040f18f960c1f
ilyana19 <aries_2152@hotmail.com>
2018-07-24 10:52:38 -0400

    added ex1

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+# Create an emotions hash, where the keys are the names of different human emotions and the values are the degree to which the emotion is being felt on a scale from 1 to 3.
+
+emotions = {
+  happiness: 3,
+  hunger: 1
+  fatigue: 2
+}
